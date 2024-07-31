440c73602769e2bb6306f1158f9e54382673426c
shivam chaturvedi <chaturvedishivam598@gmail.com>
2024-07-31 20:04:19 +0530

    custom json parser and url dispatcher added

--- app.cpp
+++ app.cpp
@@ -4,7 +4,8 @@
 #include<cppcms/applications_pool.h>
 #include<cppcms/http_response.h>
 #include<fstream>
-#include "parse.cpp";
+#include<string>
+#include "parse.cpp"
 
 using namespace std;
 
@@ -17,12 +18,17 @@ class App: public cppcms::application{
 		try{
 		fstream file("templates/"+path);
 		if(!file.is_open()){
-			cerr<<flush<<"File Not Found !!"<<endl;
+			if(path==error_path){
+				return "<h1>Page Not Found !!</h1><br><h2>To Make Your Custom Error Page make a error.html file in templates folder</h2>";
+			}
 			throw "";
 		}
 		string s;
 		char ch;
 		while(file.get(ch)){
+			if(ch=='\n'){
+				// continue;
+			}
 			s+=ch;
 		}
 		return s;
@@ -35,29 +41,34 @@ class App: public cppcms::application{
 
 	public:
 
-	App(cppcms::service &srv):cppcms::application(srv){
-		cout<<"Server Started on 127.0.0.1:8080"<<endl;
-	}
+	App(cppcms::service &srv):cppcms::application(srv){}
 
 	virtual void main(string url){
-			if(url=="/"){
-				response().out()<<render("index.html");
-			}
-			else if(url=="/hello"){
-				string urls=render("urls.json");
-				
-			}
-			else{
-				response().out()<<render(error_path);
-			}
+		auto urls=parse_json("urls.json");
+		string path=urls[url];
+		string html=render(path);
+		if(path.find(".css")!=path.npos){
+			response().content_type("text/css");
+		}
+		response().out()<<html;
+		cout<<endl<<"sending "<<html.size()<<" bytes";
 	}
 
 };
 
 
 int main(int argc ,char **argv){
+	try{
 	cppcms::service s(argc,argv);
 	s.applications_pool().mount(cppcms::applications_factory<App>());
+	string ip=s.settings().get<string>("service.ip");
+	int port=s.settings().get<int>("service.port");
+	
+	cout<<endl<<"Server Started on "<<ip<<":"<<port<<endl;
 	s.run();
+	}
+	catch(exception &r){
+		cout<<r.what();
+	}
 	return 0;
 }
